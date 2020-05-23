435c25396aed9cb72c8124178c8d8fbe836c51a6
brotherhui521 <brotherhui521@gmail.com>
2020-05-23 15:08:52 -0700

    LoadingComponent Added and Activity Indicator

--- components/MenuComponent.js
+++ components/MenuComponent.js
@@ -1,43 +1,52 @@
-import React, { Component } from 'react';
-import { Text, View, FlatList } from 'react-native';
-import { ListItem } from 'react-native-elements';
-import Dishdetail from './DishdetailComponent';
-import { connect } from 'react-redux';
-import { baseUrl } from '../shared/baseUrl';
+import React, { Component } from "react";
+import { Text, View, FlatList } from "react-native";
+import { ListItem } from "react-native-elements";
+import Dishdetail from "./DishdetailComponent";
+import { connect } from "react-redux";
+import { baseUrl } from "../shared/baseUrl";
+import{Loading} from './LoadingComponent';
 
-const mapStateToProps = state => {
-    return {
-      dishes: state.dishes
-    }
-  }
+const mapStateToProps = (state) => {
+  return {
+    dishes: state.dishes,
+  };
+};
 
 class Menu extends Component {
-    
-    render(){
-        const renderMenuItem = ({item, index}) => {
-            return (
-                <ListItem
-                key={index}
-                title={item.name}
-                subtitle={item.description}
-                hideChevron={true}
-                onPress={()=>navigate('Dishdetail',{dishId: item.id})}
-                leftAvatar={{ source: {uri:baseUrl+item.image}}}
-              />
-            );
-        }
-    
-        const {navigate} =this.props.navigation;
-        return (
-            
-            <FlatList 
-                data={this.props.dishes.dishes}
-                renderItem={renderMenuItem}
-                keyExtractor={item => item.id.toString()}
-            />
-        );
-    }
-}
- 
-export default connect(mapStateToProps)(Menu);
+  render() {
+    const renderMenuItem = ({ item, index }) => {
+      return (
+        <ListItem
+          key={index}
+          title={item.name}
+          subtitle={item.description}
+          hideChevron={true}
+          onPress={() => navigate("Dishdetail", { dishId: item.id })}
+          leftAvatar={{ source: { uri: baseUrl + item.image } }}
+        />
+      );
+    };
 
+    const { navigate } = this.props.navigation;
+
+    if (this.props.dishes.isLoading) {
+      return <Loading />;
+    } else if (this.props.dishes.errMess) {
+      return (
+        <View>
+          <Text>{this.props.dishes.errMess}</Text>
+        </View>
+      );
+    } else {
+      return (
+        <FlatList
+          data={this.props.dishes.dishes}
+          renderItem={renderMenuItem}
+          keyExtractor={(item) => item.id.toString()}
+        />
+      );
+    }
+  }
+}
+
+export default connect(mapStateToProps)(Menu);
